c1b13e8c4190851771a7ee09d96d89877469bb3d
SpirtCrazy <415131215@qq.com>
2017-08-22 08:42:46 +0800

    finish search

--- src/main/java/Engine/BPlusTree/NodeImpl.java
+++ src/main/java/Engine/BPlusTree/NodeImpl.java
@@ -31,8 +31,33 @@ public class NodeImpl implements Node{
             children = new ArrayList<Node>();
         }
     }
+    public NodeImpl(boolean leaf, boolean root) {
+        this.leaf = leaf;
+        this.root = root;
+    }
     public Object search(Comparable key) {
-
+        if (leaf) {
+            for (Map.Entry<Comparable, Object> entry : entries) {
+                if (entry.getKey().compareTo(key) == 0) {
+                    return entry.getValue();
+                }
+            }
+            return null;
+        } else {
+            if (key.compareTo(entries.get(0).getKey()) <= 0) {
+                return children.get(0).search(key);
+            } else if (key.compareTo(entries.get(entries.size() - 1).getKey()) >= 0) {
+                return children.get(children.size() - 1).search(key);
+            } else {
+                for (int i = 0; i < entries.size();i++) {
+                    if (entries.get(i).getKey().compareTo(key) <= 0 &&
+                            entries.get(i + 1).getKey().compareTo(key) >0) {
+                        return children.get(i).search(key);
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     public void remove(Comparable key) {
