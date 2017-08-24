de4fed4bac3bd6a9ff471b9a8a080e943ffc0164
SpirtCrazy <415131215@qq.com>
2017-08-24 08:38:18 +0800

    finish remove

--- src/main/java/Engine/BPlusTree/Node.java
+++ src/main/java/Engine/BPlusTree/Node.java
@@ -1,6 +1,7 @@
 package Engine.BPlusTree;
 
 import Engine.Bplustree;
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
 
 import java.util.*;
 
@@ -140,13 +141,191 @@ public class Node {
                         Map.Entry<Comparable, Object> entry = previous.getEntries().get(preSize - 1);
                         previous.getEntries().remove(entry);
 
+                        entries.add(0, entry);
+                        remove(key);
+                    } else if (next != null &&
+                            next.getEntries().size() > bpt.getOrder() / 2 &&
+                            next.getEntries().size() > 2 &&
+                            next.getParent() == parent) {
+                        Map.Entry<Comparable, Object> entry = next.getEntries().get(0);
+                        next.getEntries().remove(entry);
+
+                        entries.add(entry);
+                        remove(key);
+                    } else {
+                        if (previous != null &&
+                                (previous.getEntries().size() <= bpt.getOrder() / 2 ||
+                                previous.getEntries().size() <= 2) &&
+                                previous.getParent() == parent) {
+                            //同前面的结点合并
+                            for (int i = previous.getEntries().size() - 1;
+                                    i >= 0;i--) {
+                                entries.add(0, previous.getEntries().get(i));
+                            }
+                            remove(key);
+                            previous.setParent(null);
+                            previous.setEntries(null);
+                            parent.getChildren().remove(previous);
+
+                            //更新链表
+                            if (previous.getPrevious() != null) {
+                                Node tmp = previous;
+                                tmp.getPrevious().setNext(this);
+                                previous = tmp.getPrevious();
+                                tmp.setPrevious(null);
+                                tmp.setNext(null);
+                            } else {
+                                bpt.setHead(this);
+                                previous.setNext(null);
+                                previous = null;
+                            }
+                        } else if (next != null &&
+                                (next.getEntries().size() <= bpt.getOrder() / 2 ||
+                                next.getEntries().size() <= 2) &&
+                                next.getParent() == parent) {
+                            for (int i = 0; i < next.getEntries().size();i++) {
+                                entries.add(next.getEntries().get(i));
+                            }
+                            remove(key);
+                            next.setParent(null);
+                            next.setEntries(null);
+                            parent.getChildren().remove(next);
+
+                            if (next.getNext() != null) {
+                                Node tmp = next;
+                                tmp.getNext().setPrevious(this);
+                                next = tmp.getNext();
+                                tmp.setPrevious(null);
+                                tmp.setNext(null);
+                            } else {
+                                next.setPrevious(null);
+                                next = null;
+                            }
+                        }
+                    }
+                }
+            }
+            parent.updateRemove(bpt);
+        } else {
+            if (key.compareTo(entries.get(0).getKey()) <= 0) {
+                children.get(0).remove(key, bpt);
+            } else if (key.compareTo(entries.get(entries.size() - 1).getKey()) >= 0) {
+                children.get(children.size() - 1).remove(key, bpt);
+            } else {
+                for (int i = 0; i < entries.size();i++) {
+                    if (entries.get(i).getKey().compareTo(key) <= 0 &&
+                            entries.get(i + 1).getKey().compareTo(key) > 0) {
+                        children.get(i).remove(key,bpt);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 调整结点
+     * @param bpt
+     */
+    public void updateRemove(Bplustree bpt) {
+        validate(this, bpt);
+
+        if (children.size() < bpt.getOrder() / 2 || children.size() < 2) {
+            //子结点个数少于M / 2 或者小于2 ，则需要合并结点
+            if (root) {
+                if (children.size() >= 2) {
+                    return;
+                } else {
+                    //合并
+                    Node root = children.get(0);
+                    bpt.setRoot(root);
+                    root.setParent(null);
+                    root.setRoot(true);
+                    setEntries(null);
+                    setChildren(null);
+                }
+            } else {
+                int currentIndex = parent.getChildren().indexOf(this);
+                int preIndex = currentIndex - 1;
+                int nextIndex = currentIndex + 1;
+                Node previous = null, next = null;
+
+                if (preIndex >= 0) {
+                    previous = parent.getChildren().get(preIndex);
+                }
+                if (nextIndex < parent.getChildren().size()) {
+                    next = parent.getChildren().get(nextIndex);
+                }
+
+                if (previous != null &&
+                        previous.getChildren().size() > bpt.getOrder() / 2 &&
+                        previous.getChildren().size() > 2) {
+                    int idx = previous.getChildren().size() - 1;
+                    Node borrow =previous.getChildren().get(idx);
+                    previous.getChildren().remove(idx);
+                    borrow.setParent(this);
+                    children.add(0, borrow);
+                    validate(previous, bpt);
+                    validate(this, bpt);
+                    parent.updateRemove(bpt);
+
+                } else if (next != null &&
+                        next.getChildren().size() > bpt.getOrder() / 2 &&
+                        next.getChildren().size() > 2) {
+                    Node borrow = next.getChildren().get(0);
+                    next.getChildren().remove(0);
+                    borrow.setParent(this);
+                    children.add(borrow);
+                    validate(next, bpt);
+                    validate(this, bpt);
+                    parent.updateRemove(bpt);
+                } else {
+                    //合并结点
+                    if (previous != null &&
+                            (previous.getChildren().size() <= bpt.getOrder() / 2 ||
+                            previous.getChildren().size() <= 2)) {
+                        //同前面的结点合并
+                        for (int i = previous.getChildren().size() - 1; i >= 0;i--) {
+                            Node child = previous.getChildren().get(i);
+                            children.add(0, child);
+                            child.setParent(this);
+                        }
+                        previous.setChildren(null);
+                        previous.setEntries(null);
+                        previous.setParent(null);
+                        parent.getChildren().remove(previous);
+                        validate(this, bpt);
+                        parent.updateRemove(bpt);
+                    } else if (next != null
+                            && (next.getChildren().size() <= bpt.getOrder() / 2 ||
+                    next.getChildren().size() <= 2)) {
+                        for (int i = 0; i < next.getChildren().size();i++) {
+                            Node child = next.getChildren().get(i);
+                            children.add(child);
+                            child.setParent(this);
+                        }
+                        next.setChildren(null);
+                        next.setEntries(null);
+                        next.setParent(null);
+                        parent.getChildren().remove(next);
+                        validate(this, bpt);
+                        parent.updateRemove(bpt);
                     }
                 }
             }
         }
     }
     public void remove(Comparable key) {
-
+        int index = -1;
+        for (int i = 0; i < entries.size();i++) {
+            if (entries.get(i).getKey().compareTo(key) == 0) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            entries.remove(index);
+        }
     }
 
     /**
