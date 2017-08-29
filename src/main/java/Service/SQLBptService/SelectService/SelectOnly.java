c7fb0f87e53fe675807af62c5b26ca6ebf769e7a
SpirtCrazy <415131215@qq.com>
2017-08-29 23:56:07 +0800

    select

--- src/main/java/Service/SQLBptService/SelectService/SelectOnly.java
+++ src/main/java/Service/SQLBptService/SelectService/SelectOnly.java
@@ -8,6 +8,7 @@ import Models.Value;
 import Parser.Builder.SQLSelectBuilder;
 import Utils.ServiceUtils;
 import Exception.SelectException;
+import Utils.StringUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,9 +32,27 @@ public class SelectOnly implements SelectService {
         if (!ServiceUtils.checkColumn(columns, standerCol)) {
             throw new SelectException(SQLErrorCode.SQL00042);
         }
+
+        List<Row> rows = table.getRows();
+
+        List<Integer> indexList = new ArrayList<Integer>();
+        //获取串值的index
         for (String column : columns) {
+            for (int i = 0; i < standerCol.size();i++) {
+                if (StringUtils.equals(column,standerCol.get(i).getName())) {
+                    indexList.add(i);
+                    break;
+                }
+            }
             valuesMap.put(column, new ArrayList<Value>());
         }
-        List<Row> rows = table.getRows();
+
+        for (Row row : rows) {
+            List<Value> values = row.getValues();
+            for (Integer integer : indexList) {
+                String colName = values.get(integer).getColumName();
+
+            }
+        }
     }
 }
