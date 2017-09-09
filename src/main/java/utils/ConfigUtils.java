6bf9e06cbde9fe0a331601cc7f42b6fcc6e8f622
SpirtCrazy <415131215@qq.com>
2017-09-09 10:27:42 +0800

    make capacity and free sections configurable

--- src/main/java/utils/ConfigUtils.java
+++ src/main/java/utils/ConfigUtils.java
@@ -18,6 +18,14 @@ public class ConfigUtils {
     private String absolutePath;
     private String cacheSize;
 
+    private String capacitySmall;
+    private String capacityMedium;
+    private String capacityLarge;
+
+    private String freeBlockSmall;
+    private String freeBlockMedium;
+    private String freeBlockLarge;
+
     private static ConfigUtils config;
     private Properties properties;
 
@@ -72,6 +80,36 @@ public class ConfigUtils {
         if (!StringUtils.isBlank(CACHE_SIZE)) {
             this.cacheSize = CACHE_SIZE;
         }
+
+        String CAPACITY_SMALL = pro.getProperty("capacity-small");
+        if (!StringUtils.isBlank(CAPACITY_SMALL)) {
+            this.capacitySmall = CAPACITY_SMALL;
+        }
+
+        String CAPACITY_MEDIUM = pro.getProperty("capacity-medium");
+        if (!StringUtils.isBlank(CAPACITY_MEDIUM)) {
+            this.capacityMedium = CAPACITY_MEDIUM;
+        }
+
+        String CAPACITY_LARGE = pro.getProperty("capacity-large");
+        if (!StringUtils.isBlank(CAPACITY_LARGE)) {
+            this.capacityLarge = CAPACITY_LARGE;
+        }
+
+        String FREE_BLOCK_SMALL = pro.getProperty("free-block-small");
+        if (!StringUtils.isBlank(FREE_BLOCK_SMALL)) {
+            this.freeBlockSmall = FREE_BLOCK_SMALL;
+        }
+
+        String FREE_BLOCK_MEDIUM = pro.getProperty("free-block-medium");
+        if (!StringUtils.isBlank(FREE_BLOCK_MEDIUM)) {
+            this.freeBlockMedium = FREE_BLOCK_MEDIUM;
+        }
+
+        String FREE_BLOCK_LARGE = pro.getProperty("free-block-large");
+        if (!StringUtils.isBlank(FREE_BLOCK_LARGE)) {
+            this.freeBlockLarge = FREE_BLOCK_LARGE;
+        }
     }
 
     public void setPageSize(String pageSize) {
@@ -106,6 +144,54 @@ public class ConfigUtils {
         return cacheSize;
     }
 
+    public String getCapacitySmall() {
+        return capacitySmall;
+    }
+
+    public void setCapacitySmall(String capacitySmall) {
+        this.capacitySmall = capacitySmall;
+    }
+
+    public String getCapacityMedium() {
+        return capacityMedium;
+    }
+
+    public void setCapacityMedium(String capacityMedium) {
+        this.capacityMedium = capacityMedium;
+    }
+
+    public String getCapacityLarge() {
+        return capacityLarge;
+    }
+
+    public void setCapacityLarge(String capacityLarge) {
+        this.capacityLarge = capacityLarge;
+    }
+
+    public String getFreeBlockSmall() {
+        return freeBlockSmall;
+    }
+
+    public void setFreeBlockSmall(String freeBlockSmall) {
+        this.freeBlockSmall = freeBlockSmall;
+    }
+
+    public String getFreeBlockMedium() {
+        return freeBlockMedium;
+    }
+
+    public void setFreeBlockMedium(String freeBlockMedium) {
+        this.freeBlockMedium = freeBlockMedium;
+    }
+
+    public String getFreeBlockLarge() {
+        return freeBlockLarge;
+    }
+
+    public void setFreeBlockLarge(String freeBlockLarge) {
+        this.freeBlockLarge = freeBlockLarge;
+    }
+
     public static void main(String args[]) {
         //加载配置文件
         ConfigUtils.getConfig().loadPropertiesFromSrc();
@@ -113,5 +199,17 @@ public class ConfigUtils {
         System.out.println(ConfigUtils.getConfig().getPageSize());
 
         System.out.println(ConfigUtils.getConfig().getCacheSize());
+
+        System.out.println(ConfigUtils.getConfig().getCapacitySmall());
+
+        System.out.println(ConfigUtils.getConfig().getCapacityMedium());
+
+        System.out.println(ConfigUtils.getConfig().getCapacityLarge());
+
+        System.out.println(ConfigUtils.getConfig().getFreeBlockSmall());
+
+        System.out.println(ConfigUtils.getConfig().getFreeBlockMedium());
+
+        System.out.println(ConfigUtils.getConfig().getFreeBlockLarge());
     }
 }
